dee8dab5d2ea62c47aa00348de51966be45a7a8d
Luciano Nooijen <luciano@bytecode.nl>
2019-02-16 14:09:56 +0100

    Parsed data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import X2JS from 'x2js';
 import './App.css';
 
-const feedUrl = 'https://www.kiyoh.nl/xml/recent_company_reviews.xml?connectorcode=ee4810d178ce427d97a&company_id=4235&reviewcount=all&showextraquestions=1';
-
-const App = () => {
-    return (<div>Test</div>);
+const feedRequest = () => {
+    const response = axios({
+        method: 'get',
+        url: 'https://www.kiyoh.nl/xml/recent_company_reviews.xml',
+        params: {
+            connectorcode: 'ee4810d178ce427d97a',
+            company_id: '4235',
+            reviewcount: 'all',
+            showextraquestions: '1'
+        }
+    });
+    return response;
 };
+
+const getFeedJson = async () => {
+    const x2js = new X2JS();
+    const xmlResponse = await feedRequest();
+    const xmlData = xmlResponse.data;
+    const jsonData = x2js.xml2js(xmlData);
+    return jsonData;
+};
+
+class App extends Component {
+    constructor() {
+        super();
+        this.loadDataIntoState = this.loadDataIntoState.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadDataIntoState();
+    }
+
+    async loadDataIntoState() {
+        const jsonData = await getFeedJson();
+        const averages
+            = jsonData.default.company.average_scores.questions.question;
+        this.setState({ scores: averages });
+    }
+
+    render() {
+        return (
+            <div>Working!</div>
+        );
+    }
+}
 
 export default App;
